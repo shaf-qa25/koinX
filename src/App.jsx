a9be170917ac66012bcf21ce97d61570712ca9bf
Shafqa Fatma <fatmashafqa4@gmail.com>
2026-04-17 15:36:47 +0530

    log fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import TaxHarvesting from './pages/TaxHarvesting';
+// import TaxHarvesting from './pages/TaxHarvesting';
 
 function App() {
   return (
